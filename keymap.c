5f55e4c11bc0113e8bbfadee2d3d64900a4c6c98
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-12-05 22:51:00 +0100

    last changes to keyboard

--- keymap.c
+++ keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  º/ª | |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   Ñ  | tilde  |
- * |--------+------+------+------+------+------|  (   |           |  )   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| home |           | end  |------+------+------+------+------+--------|
  * |   LSft |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   -  |  RSft  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | CTRL | ALT  | [    |   ]  |change|                                | LEFT | DOWN |  UP  |RIGHT |  ¡¿   |
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |  +   |       |  -   |        |      |
  *                                 |SPACE |ENTER |------|       |------|   ;    | SPACE|
- *                                 |      |      |  *   |       |  _   |        |      |
+ *                                 |      |      | win  |       | Del  |        |      |
  *                                 `--------------------'       `----------------------'
  *  CAG = CTRL-ALT-GUI
  *   CA = CTRL-ALT
@@ -59,20 +59,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,       KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_TAB,
           KC_GRAVE,        KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   M(OBRACE),
           KC_RALT,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
-          KC_LSFT,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LSFT(KC_8),
-          KC_LCTL,KC_LALT,M(OBRACK),M(CBRACK),KC_FN1,
+          KC_LSFT,      KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_HOME,
+          KC_LCTL,KC_LALT, KC_LBRC, KC_RBRC, KC_FN1,
                                                                     KC_NONUS_BSLASH,  KC_RPRN,
                                                                       ALL_T(KC_RBRACKET),
-                                                                      KC_SPC,KC_ENT,KC_PAST,
+                                                                      KC_SPC,KC_ENT,KC_LGUI,
           // right hand
                       KC_BSPC,    KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   LSFT(KC_2),
                       M(CBRACE),   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,  KC_MINUS,
                                  KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,CTL_T(KC_QUOTE),
-                      LSFT(KC_9),   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
+                      KC_END,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,
                                          KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_EQL,
           KC_PSLS, LSFT(KC_NONUS_BSLASH),
           KC_LGUI,
-          LSFT(KC_SLSH),LSFT(KC_COMM), KC_SPC
+          KC_DELT,LSFT(KC_COMM), KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -90,8 +90,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        ,-------------.       ,-------------.
  *                                        |Animat|      |       |Toggle|Solid |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |Bright|Bright|      |       |      |Hue-  |Hue+  |
- *                                 |ness- |ness+ |------|       |------|      |      |
+ *                                 |      |      |      |       |      |Hue-  |Hue+  |
+ *                                 | Vol+ | Vol- | Mute |       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_FN1,
                                        RGB_MOD,KC_TRNS,
                                                KC_TRNS,
-                               RGB_VAD,RGB_VAI,KC_TRNS,
+                               KC_VOLU, KC_VOLD, KC_MUTE,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
